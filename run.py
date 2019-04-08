3fb4265a47b8d2c8c977cbbfef2a8164549047fe
jasonmuchiri <jasonmkinyua@gmail.com>
2019-04-08 17:52:02 +0300

    add the del-password and display passwords in run.py

--- run.py
+++ run.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python3.6 
 from password import Password
+    
+def create_password(user_name,pass_word):
+    '''
+    Function to create a new password
+    '''
+    new_password = Password(user_name,pass_word)
+    return new_password
 
-  def create-password(username,password):
-      '''
-      Function to create a new password
-      '''
-      new_password = Password(usename,password)
-      return new_password
+def save_passwords(password):
+    '''
+     Function to save passwords
+    '''
+    password.save_password()
 
-  def save_passwords(password):
-      '''
-      Function to save passwords
-      '''
-      password.save_password()
+def del_password(password):
+     '''
+     Function to delete a contact
+    '''
+     password.delete_password()
+
+def check_existing_passwords(user_name):
+    '''
+    Function that check if a password exists with that username and return a Boolean
+    '''
+    return Password.password_exist(user_name)
+
+def display_passwords():
+    '''
+    Function that returns all the saved passwords
+    '''
+    return Password.display_passwords()
+
+def main():
+    print("Hello Welcome to your password locker.What is your name?")
+       
+      
+
+  
